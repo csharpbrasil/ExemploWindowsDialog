899719c8f7653cdc8c6f1c82295e5d0746e0e7cf
Raphael A. F. Cardoso <ferronicardoso@outlook.com>
2020-11-04 09:02:10 -0300

    Exemplo de uso do ResultDialog

---
+++ src/ExemploWindowsDialog/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploWindowsDialog
+{
+    public partial class Form2 : Form
+    {
+        public Cliente ClienteSelecionado { get; internal set; }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            var lstClientes = new List<Cliente>
+            {
+                new Cliente { Id = 1, Nome = "João" },
+                new Cliente { Id = 2, Nome = "Carlos" },
+                new Cliente { Id = 3, Nome = "Pedro" },
+                new Cliente { Id = 4, Nome = "Marcos" },
+                new Cliente { Id = 5, Nome = "Roger" },
+            };
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = lstClientes;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                var cliente = (Cliente)dataGridView1.Rows[e.RowIndex].DataBoundItem;
+                this.ClienteSelecionado = cliente;
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
---
+++ src/ExemploWindowsDialog/UserControl1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploWindowsDialog
+{
+    public partial class UserControl1 : UserControl    
+    {   
+        public Cliente ClienteSelecionado { get; set; }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2();
+            DialogResult result = form2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                var cliente = form2.ClienteSelecionado;
+                textBox1.Text = cliente.Nome;
+            }
+            else
+            {
+                MessageBox.Show("Nada foi selecionado");
+            }
+        }
+    }
+}
